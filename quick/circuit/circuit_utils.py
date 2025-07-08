25a30826f42b0c9d3e51af166cdd0a818cb9c3f2
ACE07-Sev <a.c.e8009aiden@gmail.com>
2025-07-09 01:37:21 +0800

    - Additional prints to pinpoint divergence.

--- quick/circuit/circuit_utils.py
+++ quick/circuit/circuit_utils.py
@@ -39,11 +39,11 @@ Defined directly as complex numbers to avoid floating point errors
 """
 SQRT2 = 1/np.sqrt(2)
 
-RZ_PI2_00 = complex(
-    SQRT2, SQRT2
+RZ_PI2_00 = np.complex128(
+    SQRT2 + SQRT2 * 1j
 )
-RZ_PI2_11 = complex(
-    SQRT2, -SQRT2
+RZ_PI2_11 = np.complex128(
+    SQRT2 - SQRT2 * 1j
 )
 
 EPSILON = 1e-10
@@ -186,16 +186,27 @@ def extract_uvr_matrices(
     `r` : NDArray[np.complex128]
         The diagonal matrix r.
     """
+    print("a:", a)
+    print("b:", b)
+
     # Hermitian conjugate of b (Eq 6)
     X = a @ b.conj().T
+
+    print("X:", X)
 
     # Determinant and phase of x
     det_X = np.linalg.det(X)
     X_11 = X[0, 0] / np.sqrt(det_X)
     phi = np.angle(det_X)
 
+    print("det_X:", det_X)
+    print("X_11:", X_11)
+    print("phi:", phi)
+
     # Compute the diagonal matrix r
     arg_X_11 = np.angle(X_11)
+
+    print("arg_X_11:", arg_X_11)
 
     # The implementation of the diagonal matrix r is
     # given below, but it can be chosen freely
